e4bb22b47e465b39b542d73e5228984310fb9c3c
abluewish <35025796+abluewish@users.noreply.github.com>
2019-03-02 19:01:33 -0500

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,13 +1,32 @@
-package com.example.button;
+package com.example.helloapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.view.View;
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
+    int quantity=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void submitOrder(View view){
+        display(quantity++);
+    }
+
+    public void increment(View view){
+        quantity=quantity+1;
+        display(quantity);
+    }
+
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
 }
